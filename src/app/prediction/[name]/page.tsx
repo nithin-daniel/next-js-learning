ba634682d02834a87f92c5d4e7ceafc7d324e921
Nithin Daniel <nithindaniel2018@gmail.com>
2024-06-09 23:30:20 +0530

    dynamic route with navigator

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,9 +1,11 @@
-export default function Page({params}:any) {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        {" "}
-        {params.name}
-      </main>
-    );
-  }
-  
+interface Params {
+  params: { name: string }
+}
+export default function Page({ params }: Params) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {" "}
+      {params.name}
+    </main>
+  );
+}
