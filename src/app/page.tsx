cddb0181db8e0c9b72c76f0df5814bd648a77a51
Nithin Daniel <nithindaniel2018@gmail.com>
2024-06-09 23:29:07 +0530

    dynamic route with navigator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useState, FormEvent } from "react";
+import { useRouter } from 'next/navigation'
 export default function Home() {
   const [inputVal, setInputVal] = useState("")
+  const { push } = useRouter()
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
+    push(`/prediction/${inputVal}`)
   }
   return (
     <div>
@@ -12,7 +15,7 @@ export default function Home() {
         <h1>Enter your name</h1>
       </div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="" placeholder="Type your name..." value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
+        <input className="text-black" type="text" name="" placeholder="Type your name..." value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
         <button type="submit">Predict Data</button>
       </form>
     </div>
