ce4a9b2c8145a4d96a4aef4255c8467a99b92856
Nithin Daniel <nithindaniel2018@gmail.com>
2024-06-09 23:46:36 +0530

    added styles

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -24,12 +24,20 @@ export default async function Page({ params }: Params) {
   const [age, gender, country] = await Promise.all([ageData, genderData, countryData])
 
   return (
-    <div>
-      <div>
-        <div>Personal Info</div>
-        <div>Age:{age?.age}</div>
-        <div>Gender:{gender?.gender}</div>
-        <div>Country:{country?.country[0]?.country_id}</div>
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+      <div className="p-8">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          Personal Info
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Age: {age?.age}
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Gender: {gender?.gender}
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Nationality: {country?.country[0]?.country_id}
+        </div>
       </div>
     </div>
   );
